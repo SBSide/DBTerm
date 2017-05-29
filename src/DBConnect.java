bb8c63bd531a65be4e13c9ace39d8d731e8282d1
Side <ivecis@live.co.kr>
2017-05-29 13:27:32 +0900

    init

--- src/DBConnect.java
+++ src/DBConnect.java
@@ -23,27 +23,10 @@ public class DBConnect {
     }
 
     public void execute_query() throws SQLException {
-        String sqlStr = "SELECT avg(speed) FROM pc";
+        String sqlStr = "SELECT model, speed, hd FROM pc " +
+                "WHERE (cd = '6x' or cd = '8x') and price < 2000";
         PreparedStatement stmt = dbTest.prepareStatement(sqlStr);
         ResultSet rs = stmt.executeQuery();
-        System.out.println("문제1. pc의 평균 속력을 구하라.");
-        while(rs.next()) {
-            System.out.println("avg(speed): " + rs.getString("AVG(SPEED)"));
-        }
-        System.out.println("---------------------------");
-        sqlStr = "SELECT price FROM pc " +
-                "WHERE price >= 2000";
-        stmt = dbTest.prepareStatement(sqlStr);
-        rs = stmt.executeQuery();
-        System.out.println("문제2. pc에서 price가 2000이상인 가격을 구하라.");
-        while(rs.next()) {
-            System.out.println("price: " + rs.getString("PRICE"));
-        }
-        System.out.println("---------------------------");
-        sqlStr = "SELECT model, speed, hd FROM pc " +
-                "WHERE (cd = '6x' or cd = '8x') and price < 2000";
-        stmt = dbTest.prepareStatement(sqlStr);
-        rs = stmt.executeQuery();
         System.out.println("문제3. 6배속이나 8배속의 CD를 가지고 있으며 가격이 " +
                 "$2000 미만인 PC들의 모델 번호, 속도, 하드 디스크의 용량을 구하라.");
         while(rs.next()) {
