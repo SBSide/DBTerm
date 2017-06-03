ceefddbbc04690df79816db615bb9da847d2328a
Side <ivecis@live.co.kr>
2017-06-03 21:33:03 +0900

    170603

--- src/Table.java
+++ src/Table.java
@@ -11,91 +11,70 @@ public class Table extends JPanel {
 	/**
 	 * Create the panel.
 	 */
+	JLabel l1 = new JLabel("1");
+	JLabel l2 = new JLabel("2");
+	JLabel l3 = new JLabel("3");
+	JLabel l4 = new JLabel("4");
+	JLabel l5 = new JLabel("5");
+	JLabel l6 = new JLabel("6");
+	JLabel l7 = new JLabel("7");
+	JLabel l8 = new JLabel("8");
+	JLabel l9 = new JLabel("9");
+	JLabel l10 = new JLabel("10");
+	JLabel l11 = new JLabel("11");
+	JLabel l12 = new JLabel("12");
+	JLabel l13 = new JLabel("13");
+	JLabel l14 = new JLabel("14");
+	JLabel l15 = new JLabel("15");
+	JLabel l16 = new JLabel("16");
+	JLabel l17 = new JLabel("17");
+	JLabel l18 = new JLabel("18");
+	JLabel l19 = new JLabel("19");
+	JLabel l20 = new JLabel("20");
+
 	public Table() {
 		setBorder(new TitledBorder("테이블 현황"));
 		setLayout(new GridLayout(4,5));
-		
-		JLabel l1 = new JLabel("1");
 		l1.setHorizontalAlignment(SwingConstants.CENTER);
 		l1.setBackground(Color.YELLOW);
 		add(l1);
-		
-		JLabel l2 = new JLabel("2");
 		l2.setHorizontalAlignment(SwingConstants.CENTER);
 		add(l2);
-		
-		JLabel l3 = new JLabel("3");
 		l3.setHorizontalAlignment(SwingConstants.CENTER);
 		add(l3);
-		
-		JLabel l4 = new JLabel("4");
 		l4.setHorizontalAlignment(SwingConstants.CENTER);
 		add(l4);
-		
-		JLabel l5 = new JLabel("5");
 		l5.setHorizontalAlignment(SwingConstants.CENTER);
 		add(l5);
-		
-		JLabel l6 = new JLabel("6");
 		l6.setHorizontalAlignment(SwingConstants.CENTER);
 		add(l6);
-		
-		JLabel l7 = new JLabel("7");
 		l7.setHorizontalAlignment(SwingConstants.CENTER);
 		add(l7);
-		
-		JLabel l8 = new JLabel("8");
 		l8.setHorizontalAlignment(SwingConstants.CENTER);
 		add(l8);
-		
-		JLabel l9 = new JLabel("9");
 		l9.setHorizontalAlignment(SwingConstants.CENTER);
 		add(l9);
-		
-		JLabel l10 = new JLabel("10");
 		l10.setHorizontalAlignment(SwingConstants.CENTER);
 		add(l10);
-		
-		JLabel l11 = new JLabel("11");
 		l11.setHorizontalAlignment(SwingConstants.CENTER);
 		add(l11);
-		
-		JLabel l12 = new JLabel("12");
 		l12.setHorizontalAlignment(SwingConstants.CENTER);
 		add(l12);
-		
-		JLabel l13 = new JLabel("13");
 		l13.setHorizontalAlignment(SwingConstants.CENTER);
 		add(l13);
-		
-		JLabel l14 = new JLabel("14");
 		l14.setHorizontalAlignment(SwingConstants.CENTER);
 		add(l14);
-		
-		JLabel l15 = new JLabel("15");
 		l15.setHorizontalAlignment(SwingConstants.CENTER);
 		add(l15);
-		
-		JLabel l16 = new JLabel("16");
 		l16.setHorizontalAlignment(SwingConstants.CENTER);
 		add(l16);
-		
-		JLabel l17 = new JLabel("17");
 		l17.setHorizontalAlignment(SwingConstants.CENTER);
 		add(l17);
-		
-		JLabel l18 = new JLabel("18");
 		l18.setHorizontalAlignment(SwingConstants.CENTER);
 		add(l18);
-		
-		JLabel l19 = new JLabel("19");
 		l19.setHorizontalAlignment(SwingConstants.CENTER);
 		add(l19);
-		
-		JLabel l20 = new JLabel("20");
 		l20.setHorizontalAlignment(SwingConstants.CENTER);
 		add(l20);
-
 	}
-
 }
